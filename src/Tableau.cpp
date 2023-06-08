11a24131d3d41fc26e97e452657361f4c1325036
Jan Grünke <jan.gruenke@gmx.de>
2023-06-08 13:28:50 +0200

    fixed output path

--- src/Tableau.cpp
+++ src/Tableau.cpp
@@ -359,7 +359,7 @@ void Tableau::toDotFormat(std::ofstream &output) const {
 
 void Tableau::exportProof(std::string filename) const {
   // std::cout << "[Status] Export infinite proof: " << filename << ".dot" << std::endl;
-  std::ofstream file("./build/" + filename + ".dot");
+  std::ofstream file("./output/" + filename + ".dot");
   toDotFormat(file);
   file.close();
 }
