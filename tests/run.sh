eaf2259c2bc899f77fdaa8687c9d5cc7bd554c2e
Jan Grünke <jan.gruenke@gmx.de>
2023-10-23 14:40:48 +0200

    build test infrastructure

---
+++ tests/run.sh
@@ -0,0 +1,17 @@
+test() {
+    name=$1
+    result=$2
+    passed=$(./build/CatInfer "./tests/${name}" | grep $result | wc -l)
+    printf "%s\t%s\n" $name $passed
+}
+
+# no assumptions
+test test1 True
+test test2 True
+test test3 True
+test test4 True
+test test9 True
+test test2f False
+test hard True
+
+# with assumptions
