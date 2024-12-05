fb792792214f4e1b7e5adbcbd3d3ee6f428ff301
Jan Grünke <jan.gruenke@gmx.de>
2024-12-05 15:51:13 +0100

    removed debug print

--- src/parsing/LogicVisitor.cpp
+++ src/parsing/LogicVisitor.cpp
@@ -68,7 +68,6 @@ antlr4::ParseCancellationException parsingError(antlr4::ParserRuleContext *conte
   std::unordered_map<std::string, CanonicalRelation> renamedDerivedRelations;
   for (const auto &[key, relation] : derivedRelations) {
     renamedDerivedRelations.insert({namespaceString + key, relation});
-    std::cout << namespaceString + key << std::endl;
   }
   derivedRelations = renamedDerivedRelations;
 
