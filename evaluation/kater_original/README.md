98eb15c36635c53d4a4df53c4222df771b3399cc
Thomas Haas <tomy.haas@t-online.de>
2025-01-30 11:54:12 +0100

    Added original kater tests Updated script to also run kater (WIP)

--- evaluation/kater_original/README.md
+++ evaluation/kater_original/README.md
@@ -1,2 +1,4 @@
 This directory contains all the original Kater tests taken from their github repo:
 https://github.com/MPI-SWS/kater with commit id 509fe24
+
+We extracted the tests we are interested in into the folders "compilation" and "equivalence".
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,10 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RAT="${RAT:-$DIR/../cmake-build-release/rat}"
+### TODO: Put a relative path when creating an artifact
+KATER="/Users/thomashaas/ExternalTools/kater/Release/kater"
 TIMEOUT=3600
 
-### Function to execute a set of tests (given as argument)
-function exec_tests() {
+### Functions to execute a set of tests (given as argument)
+function exec_rat() {
+    echo "---------- Running RAT ----------"
     local tests=("$@")
     for test in "${tests[@]}"
     do
@@ -31,25 +34,62 @@ function exec_tests() {
     done
 }
 
+function exec_kater() {
+    echo "---------- Running KATER ----------"
+    local tests=("$@")
+    for test in "${tests[@]}"
+    do
+        extension="${test##*.}"
+        ### skip .md and .cat extensions
+        if [[ "${extension}" == "md" || "${extension}" == "cat" ]]; then
+            continue
+        fi
+        output=$(timeout "${TIMEOUT}" /usr/bin/time "${KATER}" "${test}" 2>&1)
+        exit_code=$?
+        time=$(echo "${output}" | awk '/user/ { print $3 }')
+
+        if [[ ${exit_code} -eq 0 ]]; then
+            result="True"
+        elif [[ ${exit_code} -eq 6 ]]; then
+            result="False"
+        elif [[ ${exit_code} -eq 124 ]]; then
+            time="${TIMEOUT}.00"
+            result="Timeout"
+        else
+            result="Error"
+        fi
+
+        printf "| %-40s | %-10s | %-10s |\n" "${test##*/}" "${time}" "${result}"
+    done
+}
+
 ### Actual test cases
 
 echo "================================================================="
 echo "================ Running Kater equivalence tests ================"
 echo "================================================================="
-kater_equiv=(
+equiv_rat=(
     "eq-co_1" "eq-co_2" "eq-eco-paper" "eq-ra1-ra2" "eq-ra1-ra3_1" 
     "eq-ra1-ra3_2" "eq-rc11-rc112" "eq-sc-scfm_1" "eq-sc-scfm_2" 
     "eq-tso-tsofm"
     )
-kater_equiv=("${kater_equiv[@]/%/.kat}")
-kater_equiv=("${kater_equiv[@]/#/${DIR}/../evaluation/kater_translated/tests/equivalence/}")
+equiv_rat=("${equiv_rat[@]/%/.kat}")
+equiv_rat=("${equiv_rat[@]/#/${DIR}/../evaluation/kater_translated/tests/equivalence/}")
 
-exec_tests "${kater_equiv[@]}"
+equiv_kater=(
+    "eq-co" "eq-eco-paper" "eq-ra1-ra2" "eq-ra1-ra3" "eq-rc11-rc112" 
+    "eq-sc-scfm" "eq-tso-tsofm"
+)
+equiv_kater=("${equiv_kater[@]/%/.kat}")
+equiv_kater=("${equiv_kater[@]/#/${DIR}/../evaluation/kater_original/tests/equivalence/}")
+
+exec_kater "${equiv_kater[@]}"
+exec_rat "${equiv_rat[@]}"
 
 echo "================================================================="
 echo "================ Running Kater compilation tests ================"
 echo "================================================================="
-kater_compilation=(
+comp_tests=(
     ### True
     "comp-imm-arm8" "comp-imm-tso" "comp-power-power-simpl" 
     "comp-rc11-arm8" "comp-rc11-imm" "comp-rc11-power-weak" 
@@ -58,10 +98,12 @@ kater_compilation=(
     ### False
     "comp-c11-power-simpl" "comp-c11-power-weak"
     )
-kater_compilation=("${kater_compilation[@]/%/.kat}")
-kater_compilation=("${kater_compilation[@]/#/${DIR}/../evaluation/kater_translated/tests/compilation/}")
+comp_tests=("${comp_tests[@]/%/.kat}")
+comp_tests_rat=("${comp_tests[@]/#/${DIR}/../evaluation/kater_translated/tests/compilation/}")
+comp_tests_kater=("${comp_tests[@]/#/${DIR}/../evaluation/kater_translated/tests/compilation/}")
 
-exec_tests "${kater_compilation[@]}"
+exec_kater "${comp_tests_kater[@]}"
+exec_rat "${comp_tests_rat[@]}"
 
 echo "================================================================="
 echo "============== Running generic memory model tests ==============="
@@ -72,11 +114,11 @@ mm_tests=(
     )
 mm_tests=("${mm_tests[@]/#/${DIR}/../evaluation/}")
 
-exec_tests "${mm_tests[@]}"
+exec_rat "${mm_tests[@]}"
 
 echo "================================================================="
 echo "====================== Running LKMM tests ======================="
 echo "================================================================="
 lkmm_tests=("${DIR}/../evaluation/lkmm/*")
 
-exec_tests "${lkmm_tests[@]}"
+exec_rat "${lkmm_tests[@]}"
